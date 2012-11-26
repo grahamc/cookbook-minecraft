d294e8cc053638a58d2b37b46f386b3bafb26584
Graham Christensen <gchristensen@nationalfield.org>
2012-11-25 20:54:38 -0500

    Creating the minecraft world directory before mounting.

--- recipes/volume.rb
+++ recipes/volume.rb
@@ -1,3 +1,8 @@
+directory "/var/minecraft/config/#{node['minecraft']['server']['level-name']}" do
+    mode '0755'
+    owner 'minecraft'
+end
+
 if not node['minecraft']['system']['device'].nil?
     mount "/var/minecraft/config/#{node['minecraft']['server']['level-name']}" do
         device node['minecraft']['system']['device']
